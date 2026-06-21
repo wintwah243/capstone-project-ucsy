b57e7bbb1bf1622bc9f5e92c1f6734f51fa14efd
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2026-06-21 15:23:13 +0630

    initial set up

---
+++ flood-awareness-platform/backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json()); 
+
+app.get('/', (req, res) => {
+  res.send('Flood Awareness Platform Backend is working');
+});
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Database connected'))
+  .catch((err) => console.error('Database connection failed', err));
+
+// Server 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ flood-awareness-platform/frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div class="text-3xl font-bold underline">App</div>
+  )
+}
+
+export default App
